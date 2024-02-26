ff572169c9be7b80610576a5feb7ef2240621cad
NataliaReiis <nataliareis208@gmail.com>
2024-02-26 17:48:11 -0300

    feat: database postgres

--- database-memory.js
+++ database-memory.js
@@ -4,22 +4,23 @@ export class DatabaseMemory {
   #videos = new Map();
 
   list(search) {
-    return Array.from(this.#videos.entries()).map((videoArray) => {
-      const id = videoArray[0];
-      const data = videoArray[1];
+    return Array.from(this.#videos.entries())
+      .map((videoArray) => {
+        const id = videoArray[0];
+        const data = videoArray[1];
 
-      return {
-        id,
-        ...data,
-      }
-    })
-    .filter(video => {
-      if (search) {
-        return video.title.includes(search)
-      }
+        return {
+          id,
+          ...data,
+        };
+      })
+      .filter((video) => {
+        if (search) {
+          return video.title.includes(search);
+        }
 
-      return true
-    })
+        return true;
+      });
   }
 
   create(video) {
--- database-postgres.js
+++ database-postgres.js
@@ -1,31 +1,36 @@
-import {randomUUID} from 'node:crypto'
-import {sql} from './db.js'
+import { randomUUID } from "node:crypto";
+import { sql } from "./db.js";
 
 export class DatabasePostgres {
-   async list(search){
-       let videos
+  async list(search) {
+    let videos;
 
-       if (search) {
-        videos = await sql `select * from videos where tilte ilike "%${search}"`
-       } else {
-        videos = await sql `select * from videos `
-       }
-
-       return videos
+    if (search) {
+      videos = await sql`select * from videos where title ilike ${
+        "%" + search + "%"
+      }`;
+    } else {
+      videos = await sql`select * from videos `;
     }
 
-    async create(video) {
-        const videoId = randomUUID()
-        const { title, description, duration } = videos
+    return videos;
+  }
 
-        await sql `insert into videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`
-    }
+  async create(video) {
+    const videoId = randomUUID();
 
-    update(id, video) {
+    const { title, description, duration } = video;
 
-    }
+    await sql`insert into videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`;
+  }
 
-    delete(id) {
+  async update(id, video) {
+    const { title, description, duration } = video;
 
-    }
+    await sql`update videos set title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`;
+  }
+
+  async delete(id) {
+    await sql `delete from videos where id = ${id}`
+  }
 }
